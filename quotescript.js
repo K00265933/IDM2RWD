e74af39a5ccb6411c007d56ceb60d4d10cc49884
K00265933 <K00265933@student.lit.ie>
2022-10-19 09:43:51 +0100

    Create quotescript.js
    Quotes for Generator

---
+++ quotescript.js
@@ -0,0 +1,90 @@
+[
+    {
+        "text": "Creativity is seeing what others see and thinking what no one else ever thought.",
+        "author": "Albert Einstein"
+    },
+    {
+        "text": "Don't wait for inspiration. It comes while working.",
+        "author": "Henri Matisse"
+    },
+    {
+        "text": "The worst enemy to creativity is self-doubt.",
+        "author": "Sylvia Plath"
+    },
+    {
+        "text": "Create with the heart; build with the mind.",
+        "author": "Criss Jami"
+    },
+    {
+        "text": "You can't use up creativity. The more you use, the more you have.",
+        "author": "Maya Angelou"
+    },
+    {
+        "text": "The creative process is a process of surrender, not control.",
+        "author": "Bruce Lee"
+    },
+    {
+        "text": "Creativity is a wild mind and a disciplined eye.",
+        "author": "Dorothy Parker"
+    },
+    {
+        "text": "Problems are not stop signs, they are guidelines.",
+        "author": "Robert H. Shuller"
+    },
+    {
+        "text": "Not everything that is faced can be changed. But nothing can be changed until it is faced.",
+        "author": "James Baldwin"
+    },
+    {
+        "text": "Don't bother people for help without first trying to solve the problem yourself.",
+        "author": "Colin Powel"
+    },
+    {
+        "text": "The great aim of education is not knowledge but action.",
+        "author": "Herbert Spencer"
+    },
+    {
+        "text": "Every first draft is perfect, because all a first draft has to do is exist.",
+        "author": "Jane Smiley"
+    },
+    {
+        "text": "All great changes are preceded by choas.",
+        "author": "Deepak Chopra"
+    },
+    {
+        "text": "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+        "author": "Barack Obama"
+    },
+    {
+        "text": "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.",
+        "author": "Charles Darwin"
+    },
+    {
+        "text": "We can't be afraid of change. You may feel very secure in the pond that you are in, but if you never venture out of it, you will never know that there is such a thing as an ocean, a sea.",
+        "author": "C. JoyBell C."
+    },
+    {
+        "text": "First, solve the problem. Then, write the code.",
+        "author": "John Johnson"
+    },
+    {
+        "text": "Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday's code.",
+        "author": "Dan Salomon"
+    },
+    {
+        "text": "Code is like humor. When you have to explain it, it's bad.",
+        "author": "Cory House"
+    },
+    {
+        "text": "Everything is theoretically impossible until it is done.",
+        "author": "Robert A. Heinlein"
+    },
+    {
+        "text": "We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.",
+        "author": "Stephen Hawking"
+    },
+    {
+        "text": "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.",
+        "author": "Marie Curie"
+    }
+]
